1d24de862bd1157c7ba0598bef9a6b4af70fe316
julia-888 <71026631+julia-888@users.noreply.github.com>
2023-05-29 20:13:16 +0300

    Mon, May 29, 2023  8:13:16 PM

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -7,37 +7,75 @@ import { Button } from '@mui/material';
 import { useEffect, useState } from "react";
 import Message from './Message';
 import { json } from 'stream/consumers';
-import styled from '@emotion/styled';
+import styled from 'styled-components';
+import axios from 'axios';
 
 type props = {
   setEnteredStatus: (status: boolean) => void;
   username: string;
 }
 
-export default function Chat({setEnteredStatus, username}: props) {
-  //набираемый текст сообщения
-  const [msg, setMsg] = useState('');
-  //все сообщения
-  const [messages, setMessages] = useState(Array<string>);
+interface IMessages {
+  id_of_message: number,
+  text_of_message: string,
+  sender_name: string,
+  time_of_sending: string,
+}
 
+export default function Chat({setEnteredStatus, username}: props) {
+  //объект с текущей датой и временем
+  const now = new Date();
+  //набираемый текст сообщения
+  const [msgText, setMsgText] = useState('');
+  //все сообщения
+  const [messages, setMessages] = useState<IMessages[]>([]);
+
+  //get-запросы
   useEffect(() => {
-    
-  })
+    axios
+      .get("http://localhost:5000/messages")
+      .then(data => {
+        setMessages(data.data);
+      })
+      .catch((err) => {console.log(err)})
+  }, []);
+
+  function sendData() {
+    const h = String(now.getHours()).padStart(2, '0');
+    const m = String(now.getMinutes()).padStart(2, '0');
+    const s = String(now.getSeconds()).padStart(2, '0');
+    const currentTime = h + ' : ' + m + ' : ' + s;
+    const dataForSending = {
+      text: msgText,
+      name: username,
+      time: currentTime
+    }
+    axios
+    .put("http://localhost:5000/sendMessage", {dataForSending})
+    .then(data => { setMessages(data.data); });
+            
+  }
   
   return (
     <ChatFrame>
-      <BackButton onClick={() => setEnteredStatus(false)}>Назад!</BackButton>
+      <BackButton onClick={() => {setEnteredStatus(false); console.log(messages)}}>Назад!</BackButton>
       <ChatMessages>
         <ul>
           {
-            messages.map(message => <ol>{message}</ol>)
-        }
+          messages.map( message => {return(<li>
+            <label>{message.sender_name}</label>
+            <span>      {message.text_of_message}     </span>
+            <label>{message.time_of_sending}</label>
+          </li>)})
+          }
+            
+        
         </ul>
       </ChatMessages>
         <ChatItems>
-          <ChatInput onChange={(e) => {setMsg(e.target.value)}}></ChatInput>
+          <ChatInput onChange={(e) => {setMsgText(e.target.value)}}></ChatInput>
           <ChatButton
-            onClick={() => {setMessages([...messages, msg]); console.log(messages)}}
+            onClick={() => {sendData()}}
           >Отправить!</ChatButton>
         </ChatItems>
       </ChatFrame>
